84c2d074ed99d9d73daeeb571823d0eca41cea72
sharvari <sharvarisalodkar12@gmail.com>
2024-07-24 00:34:54 +0530

    & probs day4

--- week1/Day4/FirstOccur.java
+++ week1/Day4/FirstOccur.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 class Solution {
     public int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);//returns the first index of substring
